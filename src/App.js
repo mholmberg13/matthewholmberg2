70498f36c0f3f0fdcb5f0b928d59101ed4da0b63
Matt Holmberg <mholmberg13@gmail.com>
2021-03-17 20:18:07 -0600

    Working request issues.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js'
+import OrderForm from './components/OrderForm.js'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <OrderForm/>
       </div>
     );
   }
