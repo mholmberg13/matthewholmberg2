f26c0a69f4798ecf57aca6a1d7a3675ae2d9202c
Matt Holmberg <mholmberg13@gmail.com>
2021-04-13 20:36:49 -0600

    Added Bbanner to o WebDev component.

--- src/components/WebDev.js
+++ src/components/WebDev.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
-
+import BannerImage from '../img/Web-banner.png';
+import Laptop from '../img/computer.png';
 
 class WebDev extends React.Component {
     render () {
         return (
             <div>
-                <h1>Web Development</h1>
+                <div className='web-banner-container'>
+                    <div className='web-banner-inner'>
+                        <img src={BannerImage}/>
+                        <div className='web-banner-content-container'>
+                            <div className='web-content-inner'>
+                                <h1 className='web-content-title'>Web Development</h1>
+                                <h3>
+                                    Bringing creativity and originality to your web applications and software.
+                                </h3>
+                            </div>
+                            <div className='laptop-img'>
+                                <img src={Laptop}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
