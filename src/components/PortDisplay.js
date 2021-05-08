0cfea89f0bcd8041101263a2e3b71f01bd224823
Matt Holmberg <mholmberg13@gmail.com>
2021-05-08 13:27:54 -0500

    New WebDev content added, portfolio cards started.

---
+++ src/components/PortDisplay.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class PortDisplay extends React.Component {
+    render () {
+        return (
+            <div className='port-display-container'>
+                <div className='portfolio-display'>
+                    <div className='port-card-container'>
+                        <div className='port-card'>
+                            <h2>Med-3D</h2>
+                            <p>Med-3D is a 3D Printing service that provides underfunded medical centers to order medical supplies that is 3D printed by users with home printers.</p>
+                            <a href='#'>See Github</a>
+                        </div>
+                    </div>
+                    <div className='port-card-container'>
+                        <div className='port-card'>
+                            <h2>Med-3D</h2>
+                            <p>Med-3D is a 3D Printing service that provides underfunded medical centers to order medical supplies that is 3D printed by users with home printers.</p>
+                            <a href='#'>See Github</a>
+                        </div>
+                    </div>
+                    <div className='port-card-container'>
+                        <div className='port-card'>
+                            <h2>Med-3D</h2>
+                            <p>Med-3D is a 3D Printing service that provides underfunded medical centers to order medical supplies that is 3D printed by users with home printers.</p>
+                            <a href='#'>See Github</a>
+                        </div>
+                    </div>
+                    <div className='port-card-container'>
+                        <div className='port-card'>
+                            <h2>Med-3D</h2>
+                            <p>Med-3D is a 3D Printing service that provides underfunded medical centers to order medical supplies that is 3D printed by users with home printers.</p>
+                            <a href='#'>See Github</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PortDisplay;
--- src/components/WebDev.js
+++ src/components/WebDev.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import BannerImage from '../img/Web-banner.png';
 import Laptop from '../img/computer.png';
-import Insomnia from '../img/insomnia-graphic.png';
-import Brian from '../img/Brian.png';
-import Crypto from '../img/crypto.png'
+import PortIntro from '../img/port-intro.png';
+import PortDisplay from './PortDisplay.js';
 
 class WebDev extends React.Component {
     render () {
@@ -28,8 +27,20 @@ class WebDev extends React.Component {
                 </div>
 
                 <div className='web-main'>
-                    <h1 className='port-title'>Portfolio</h1>
                     <div className='web-main-content-container'>
+                        <div className='web-intro'>
+                            <div className='intro-summary-container'>
+                                <h1 className='port-name'>Matthew Holmberg</h1>
+                                <h4>Full Stack Web Developer</h4>
+                                <div className='intro-summary'>
+                                    <p>As an exprienced Full Stack Web Developer with a focus on JavaScript, I have designed, maintained, and built web applications for a number of clients in a variety of industries. Whether your website needs redesigned or your businiess needs a web application to help streamline production, I can help.  </p>
+                                </div>
+                            </div>
+                            <div className='intro-images'>
+                                <img className='port-intro-img' src={PortIntro}/>
+                            </div>
+                        </div>
+                        <PortDisplay/>
                         <div className='skills'>
                             <h1>Skills</h1>
                             <ul>
@@ -42,15 +53,6 @@ class WebDev extends React.Component {
                                 <li>Git Management</li>
                                 <li>Flexbox</li>
                             </ul>
-                        </div>
-                        <div className='portfolio'>
-                            <div className='portfolio-images'>
-                                <img className='insomnia-img' src={Insomnia}/>
-                                <img className='crypto-img' src={Crypto}/>
-                                <div className='brian-background'>
-                                    <img className='brian-img' src={Brian}/>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
