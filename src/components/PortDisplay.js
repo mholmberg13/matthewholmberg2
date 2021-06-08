7db6c55defca192a872a62478decdeee763e440f
Matt Holmberg <mholmberg13@gmail.com>
2021-06-08 16:36:58 -0500

    ChRemoved Port border and fixed Skills and Tabs layout.

--- src/components/PortDisplay.js
+++ src/components/PortDisplay.js
@@ -28,15 +28,16 @@ class PortDisplay extends React.Component {
             <div className='port-display-container'>
                 <div className='tabs'>
                     <h3 onClick={this.handlePortChange} className='port-tab'>Portfolio</h3>
+                    <hr/>
                     <h3 onClick={this.handleSkillChange} className='port-tab'>Skills</h3>
                 </div>
-                <hr/>
+                {/* <hr className='port-tab-line'/> */}
                 {this.state.tab === 0 ? <div className='portfolio-display'>
                     <div className='port-card-container'>
                         <div className='port-card'>
                             <h2>Med-3D</h2>
                             <p>Med-3D is a 3D Printing service that provides underfunded medical centers to order medical supplies that is 3D printed by users with home printers.</p>
-                            <a href='#'>See Github</a>
+                            <a href='#'>SEE GITHUB</a>
                             <div className="tools-used">
                                 <h4>Built with:</h4>
                                 <ul>
@@ -55,7 +56,7 @@ class PortDisplay extends React.Component {
                         <div className='port-card'>
                             <h2>Print-R2</h2>
                             <p>Print-R2 is a dashboard system that helps users with 3D-Printers streamline print-on-demand services. It's client side will feature both a web and mobile application.</p>
-                            <a href='#'>See Github</a>
+                            <a href='#'>SEE GITHUB</a>
                             <div className="tools-used">
                                 <h4>Built with:</h4>
                                 <ul>
@@ -74,7 +75,7 @@ class PortDisplay extends React.Component {
                         <div className='port-card'>
                             <h2>Crypto-Tracker</h2>
                             <p>Crypto-Tracker is a web application that allows users to track crypto-currency prices based off of their own personalized settings and preferences.</p>
-                            <a href='#'>See Github</a>
+                            <a href='#'>SEE GITHUB</a>
                             <div className="tools-used">
                                 <h4>Built with:</h4>
                                 <ul>
@@ -93,7 +94,7 @@ class PortDisplay extends React.Component {
                         <div className='port-card'>
                             <h2>Cy-Log</h2>
                             <p>Cy-Log is a web application that allows users to keep a detailed scientific journal that can be accessed from anywhere. Cy-Log will soon allow users to create a secure account and encrypt the users information.</p>
-                            <a href='#'>See Github</a>
+                            <a href='#'>SEE GITHUB</a>
                             <div className="tools-used">
                                 <h4>Built with:</h4>
                                 <ul>
