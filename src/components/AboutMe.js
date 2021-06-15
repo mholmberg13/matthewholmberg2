e2e4a229590200454dfc63d07c7f32cc19b0f738
Matt Holmberg <mholmberg13@gmail.com>
2021-06-14 19:21:09 -0500

    Added About Me content and set the layout for oage.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,8 +5,13 @@ class AboutMe extends React.Component {
     render () {
         return (
             <div className='about-me'>
-                <h1>About Me</h1>
-                <p>As a Full Stack Web Developer, 3D Printing Technician, and Small Electronics Specialist I have brought together a team to provide quality work to all of our clients while pushing the limits of technology. With our combined experience and skills we plan to research and develop new technologies and creating a better world around us. </p>
+                <div className='portrait-container'>
+                        
+                </div>
+                <div className='header-bio-container'>
+                    <h1 className='about-heading'>About Me</h1>
+                    <p>As a Full Stack Web Developer, 3D Printing Technician, and Small Electronics Specialist I have brought together a team to provide quality work to all of our clients while pushing the limits of technology. With our combined experience and skills we plan to research and develop new technologies and creating a better world around us. </p>
+                </div>
             </div>
         )
     }
