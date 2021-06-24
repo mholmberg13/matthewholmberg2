2920bda70aacb8129e2566a1e5c9afa52b5efecf
Matt Holmberg <mholmberg13@gmail.com>
2021-06-24 16:02:42 -0500

    Mobile compatibility complete.

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -5,11 +5,13 @@ import ContactForm from './ContactForm';
 class ContactMe extends React.Component {
     render () {
         return (
-            <div className='contact-me'>
-                <hr/>
-                <h1>Contact Me</h1>
-                <p>Send me a message or request for a project and I will get back to you as soon as possible.</p>
-                <ContactForm/>
+            <div className='contact-me-container'>
+                <div className='contact-me'>
+                    <hr/>
+                    <h1>Contact Me</h1>
+                    <p>Send me a message or request for a project and I will get back to you as soon as possible.</p>
+                    <ContactForm/>
+                </div>
             </div>
         )
     }
