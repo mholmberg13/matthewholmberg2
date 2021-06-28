738df7c043dcbba496b160d73e21a7cbb15f19af
Matt Holmberg <mholmberg13@gmail.com>
2021-06-28 15:55:23 -0500

    Revert "Fixed final css issues."
    This reverts commit 4e69a6e88dab91f92c6886851b4ab685322ffc17.

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,32 +14,32 @@ class Main extends React.Component {
                     </div>
                     <div className="bio-outer">
                         <div className="bio-container">
-                            <a href="/3dprinting-prototyping" className="main-bio" id="print-card">
+                            <div className="main-bio" id="print-card">
                                 <h2>3D Printing</h2>
-                                <img src={image2} className="bio-pic" alt="3D Printer"/>
-                                    <p className="bio-paragraph">
+                                <img src={image2} className="bio-pic" alt="3D-Printer printing object."/>
+                                    <p>
                                         Do you have a 3D model that you would like printed? Just send us the file and we will print it for you at an affordable price! Do you need an object modeled? We can do that too! Just contact us with a description of what you need and we can give you a quote. We provide a number of materials and colors.
                                     </p>
-                                    <p className="learn-more">Learn More</p>
-                            </a>
+                                    <a href="/3dprinting-prototyping">Learn More</a>
+                            </div>
                             <div className="vert-line" id='vert-one'></div>
-                            <a href="/webdevelopment" className="main-bio" id="web-card">
+                            <div className="main-bio" id="web-card">
                                 <h2 id="bio-web-title">Web Development</h2>
                                 <img src={image1} className="web-bio-pic" alt="Code on computer screen."/>
-                                    <p className="bio-paragraph">
+                                    <p>
                                         We have designed, built, and maintained web applications, websites, and mobile applications for a variety of clients in a number of industries. Whether you are a small bussiness or a large company, we have the skills and rescources to solve your problem. 
                                     </p>
-                                    <p className="learn-more">Learn More</p>
-                            </a>
+                                    <a href="/webdevelopment">Learn More</a>
+                            </div>
                             <div className="vert-line" id='vert-two'></div>
-                            <a href="/3dprinting-prototyping" className="main-bio" id="proto-card">
+                            <div className="main-bio" id="proto-card">
                                 <h2>Prototyping</h2>
                                 <img src={image3} className="bio-pic" alt="Microcontroller close up."/>
-                                <p className="bio-paragraph">
+                                <p>
                                     Do you have an invention or an idea that you dont have the resources or know-how to build? We can help you design and build a prototype with full confidentiality. Specializing in small electronics, robotics, and mechanics, we have a team of experienced engineers ready to bring your idea to life. 
                                 </p>
-                                <p className="learn-more">Learn More</p>
-                            </a>
+                                <a href="/3dprinting-prototyping">Learn More</a>
+                            </div>
                         </div>
                     </div>
                 </div>
